9b6ab92a99a4f541b1427770f938b170d538eb54
paldepind <simonfv@gmail.com>
2018-08-24 22:26:03 +0200

    Improve TodoMVC, todo name can be edited

--- examples/todomvc/src/Main.purs
+++ examples/todomvc/src/Main.purs
@@ -3,21 +3,22 @@ module Main where
 import Prelude
 
 import Effect (Effect)
-import Hareactive (Behavior, Now, Stream, changes, filter, sample, scan, snapshot, snapshotWith, stepper)
-import Turbine (Component, modelView, output, runComponent, static, withStatic, (</>))
+import Hareactive as H
+import Turbine (Component, modelView, output, runComponent, withStatic, (</>))
 import Turbine.HTML.Elements as E
 import Web.UIEvent.KeyboardEvent as KE
 
-isEnter :: KE.KeyboardEvent -> Boolean
-isEnter = (_ == "Enter") <<< KE.key
 
-todoInput :: {} -> Component {} { clearedValue :: Behavior String, addItem :: Stream String }
+isKey :: String -> KE.KeyboardEvent -> Boolean
+isKey key event = (KE.key event) == key
+
+todoInput :: {} -> Component {} { clearedValue :: H.Behavior String, addItem :: H.Stream String }
 todoInput = modelView model view
   where
     model { keyup, value } {} = do
-      let enterPressed = filter isEnter keyup
-      clearedValue <- sample $ stepper "" ((enterPressed $> "") <> changes value)
-      let addItem = filter (_ == "") $ snapshot clearedValue enterPressed
+      let enterPressed = H.filter (isKey "Enter") keyup
+      clearedValue <- H.sample $ H.stepper "" ((enterPressed $> "") <> H.changes value)
+      let addItem = H.filter (_ == "") $ H.snapshot clearedValue enterPressed
       pure { clearedValue, addItem }
     view input =
       E.input ({ value: input.clearedValue, class: E.staticClass "new-todo" } `withStatic` {
@@ -26,39 +27,53 @@ todoInput = modelView model view
       }) `output` (\o -> { keyup: o.keyup, value: o.inputValue })
 
 type TodoItemOut =
-  { isComplete :: Behavior Boolean
-  , taskName :: Behavior String
+  { isComplete :: H.Behavior Boolean
+  , name :: H.Behavior String
+  , isEditing :: H.Behavior Boolean
   }
 
-todoItem :: { id :: Int } -> Component {} TodoItemOut
+todoItem :: { id :: Int, name :: String } -> Component {} TodoItemOut
 todoItem = modelView model view
   where
-    model input { id } = do
-      isComplete <- sample $ stepper false input.toggleTodo
-      pure { isComplete, taskName: pure "Foobar" }
+    model input options = do
+      isComplete <- H.sample $ H.stepper false input.toggleTodo
+      let cancelEditing = H.filter (isKey "Escape") input.nameKeyup
+      let finishEditing = H.filter (isKey "Enter") input.nameKeyup
+      -- Editing should stop if either on enter or on escape
+      let stopEditing = cancelEditing <> finishEditing
+      -- The name when editing started
+      initialName <- H.sample $ H.stepper "" (H.snapshot input.name input.startEditing)
+      -- When editing is canceled the name should be reset to what is was when
+      -- editing begun.
+      let cancelName = H.snapshot initialName cancelEditing
+      isEditing <- H.sample $ H.toggle false (input.startEditing) stopEditing
+      name <- H.sample $ H.stepper options.name (H.changes input.name <> cancelName)
+      pure { isComplete, name, isEditing }
     view input =
-      E.li ({ class: E.staticClass "todo" <> E.toggleClass { completed: input.isComplete } }) (
+      E.li ({ class: E.staticClass "todo" <> E.toggleClass { completed: input.isComplete, editing: input.isEditing } }) (
         E.div ({ class: (E.staticClass "view") }) (
-          E.checkbox ({ checked: input.isComplete, class: E.staticClass "toggle" }) `output` (\o -> { toggleTodo: o.checkedChange }) </>
-          E.label_ (E.textB input.taskName) `output` (\o -> { startEditing: o.dblclick }) </>
+          E.checkbox
+            ({ checked: input.isComplete
+             , class: E.staticClass "toggle"
+            }) `output` (\o -> { toggleTodo: o.checkedChange }) </>
+          E.label_ (E.textB input.name) `output` (\o -> { startEditing: o.dblclick }) </>
           E.button { class: E.staticClass "destroy" } (E.text "") `output` (\o -> { deleteClicked: o.click })
         ) </>
-        E.input ({ value: input.taskName, class: E.staticClass "edit" }) `output` (\o -> {
-          newNameInput: o.input,
+        E.input ({ value: input.name, class: E.staticClass "edit" }) `output` (\o -> {
+          name: o.inputValue,
           nameKeyup: o.keyup,
           nameBlur: o.blur
         })
-        -- actions: { focus: focusInput }
     )
 
 type TodoAppModelOut = {}
 
-type TodoAppViewOut = { addItem :: Stream String }
+type TodoAppViewOut = { addItem :: H.Stream String }
 
-todoAppModel :: TodoAppViewOut -> Unit -> Now TodoAppModelOut
+todoAppModel :: TodoAppViewOut -> Unit -> H.Now TodoAppModelOut
 todoAppModel input _ = do
-  nextId <- sample $ scan (+) 0 (input.addItem $> 1)
-  let newTodoS = snapshotWith (\name id -> { name, id }) nextId input.addItem
+  nextId <- H.sample $ H.scan (+) 0 (input.addItem $> 1)
+  let newTodoS = H.snapshotWith (\name id -> { name, id }) nextId input.addItem
   pure {}
 
 todoAppView :: TodoAppModelOut -> Component TodoAppViewOut TodoAppViewOut
@@ -68,7 +83,7 @@ todoAppView {} =
       E.h1_ (E.text "todo") </>
       todoInput {} `output` (\o -> { addItem: o.addItem }) </>
       E.ul { class: E.staticClass "todo-list" } (
-        todoItem { id: 2 }
+        todoItem { id: 2, name: "Test" }
       )
     )
   )

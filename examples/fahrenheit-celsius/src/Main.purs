52b3b112e81e1a7b149ecb27cb33cd5ed9dc5e73
Simon Friis Vindum <simonfv@gmail.com>
2019-08-31 11:40:34 +0200

    Fix bugs in ZIP code validator example and fahrenheit celsius converter

--- examples/fahrenheit-celsius/src/Main.purs
+++ examples/fahrenheit-celsius/src/Main.purs
@@ -9,9 +9,15 @@ import Hareactive.Combinators (changes, filterJust, stepper)
 import Turbine (Component, component, use, output, runComponent, (</>))
 import Turbine.HTML as E
 
+fahrenToCelsius :: Number -> Number
+fahrenToCelsius f = (f - 32.0) / 1.8
+
+celsiusToFahren :: Number -> Number
+celsiusToFahren c = (c * 9.0) / 5.0 + 32.0
+
 type TemperatureConverterOutput =
-  { fahren :: Behavior Number
-  , celsius :: Behavior Number
+  { fahren :: Behavior String
+  , celsius :: Behavior String
   }
 
 parseNumbers :: Stream String -> Stream Number
@@ -19,20 +25,18 @@ parseNumbers = filterJust <<< (map fromString)
 
 temperatureConverter :: Component TemperatureConverterOutput {}
 temperatureConverter = component \on -> do
-  let fahrenToCelsius f = (f - 32.0) / 1.8
-      celsiusToFahren c = (c * 9.0) / 5.0 + 32.0
-      celsiusNrChange = parseNumbers on.celsiusChange
+  let celsiusNrChange = parseNumbers on.celsiusChange
       fahrenNrChange = parseNumbers on.fahrenChange
-  celsius <- stepper 0.0 (celsiusNrChange <> (fahrenToCelsius <$> fahrenNrChange))
-  fahren <- stepper 0.0 (fahrenNrChange <> (celsiusToFahren <$> celsiusNrChange))
+  celsius <- stepper "0" (on.celsiusChange <> (show <$> fahrenToCelsius <$> fahrenNrChange))
+  fahren <- stepper "0" (on.fahrenChange <> (show <$> celsiusToFahren <$> celsiusNrChange))
   ( E.div {} (
       E.div {} (
         E.label {} (E.text "Fahrenheit") </>
-        E.input { value: show <$> fahren } `use` (\o -> { fahrenChange: changes o.value })
+        E.input { value: fahren } `use` (\o -> { fahrenChange: changes o.value })
       ) </>
       E.div {} (
         E.label {} (E.text "Celsius") </>
-        E.input { value: show <$> celsius } `use` (\o -> { celsiusChange: changes o.value })
+        E.input { value: celsius } `use` (\o -> { celsiusChange: changes o.value })
       )
     )
   ) `output` { fahren, celsius }
--- examples/zip-codes/src/Main.purs
+++ examples/zip-codes/src/Main.purs
@@ -2,34 +2,18 @@ module ZipCodes.Main where
 
 import Prelude
 
-import Data.Argonaut.Core as J
-import Data.Either (Either(..))
-import Data.HTTP.Method (Method(..))
-import Data.Number (fromString)
-import Data.Either (fromRight, hush)
+import Data.Either (Either(..), fromRight, hush)
 import Effect (Effect)
-import Effect.Aff (Aff, launchAff)
-import Effect.Class (liftEffect)
-import Effect.Console (log)
+import Effect.Aff (Aff)
 import Partial.Unsafe (unsafePartial)
-import Data.Tuple (Tuple(..))
-import Hareactive.Types (Behavior, Stream, Now)
-import Hareactive.Combinators (changes, split, filterJust, stepper, filter, runStreamAff)
-import Network.HTTP.Affjax as AX
-import Network.HTTP.Affjax.Response as AXRes
-import Network.HTTP.StatusCode (StatusCode(..))
+import Hareactive.Combinators (changes, split, filterJust, stepper, runStreamAff)
+import Affjax as AX
+import Affjax.ResponseFormat as ResponseFormat
+import Affjax.StatusCode (StatusCode(..))
 import Data.String.Regex (Regex, regex, test)
 import Data.String.Regex.Flags (ignoreCase)
-import Turbine (Component, modelView, use, runComponent, (</>), static)
+import Turbine (Component, component, output, use, runComponent, (</>), static)
 import Turbine.HTML as E
-
-type AppModelOut =
-  { status :: Behavior String
-  }
-
-type AppViewOut =
-  { zipCode :: Behavior String
-  }
 
 zipRegex :: Regex
 zipRegex = unsafePartial $ fromRight $ regex "^\\d{5}$" ignoreCase
@@ -37,40 +21,37 @@ zipRegex = unsafePartial $ fromRight $ regex "^\\d{5}$" ignoreCase
 isValidZip :: String -> Boolean
 isValidZip = test zipRegex
 
+apiUrl :: String
 apiUrl = "http://api.zippopotam.us/us/"
 
 fetchZip :: String -> Aff String
 fetchZip zipCode = do
-  res <- AX.get AXRes.json (apiUrl <> zipCode)
-  liftEffect $ log $ "GET, status: " <> show res.status <> ", response: " <> J.stringify res.response
-  pure
-    if res.status == StatusCode 404
-    then "Zip code does not exist"
-    else "Valid zip code for " <> "Foo"
+  res <- AX.get ResponseFormat.json (apiUrl <> zipCode)
+  pure (case res.body of
+    Left _ -> "JSON parse error"
+    Right json ->
+      if res.status == StatusCode 404
+      then "Zip code does not exist"
+      else "Valid zip code" -- TODO: Extract the city name from JSON here.
+       )
 
-zipModel { zipCode } = do
-  let
-    zipCodeChange = changes zipCode
-    Tuple validZipCodeChange invalidZipCodeChange = split isValidZip zipCodeChange
+app :: Component {} {}
+app = component \on -> do
+  let zipCodeChange = changes on.zipCode
+      { pass: validZipCodeChange, fail: invalidZipCodeChange } = split isValidZip zipCodeChange
   fetchResult <- runStreamAff $ map fetchZip validZipCodeChange
-  let
-    statusChange =
-      (invalidZipCodeChange $> "Not a valid zip code") <>
-      (validZipCodeChange $> "Loading ...") <>
-      (filterJust $ hush <$> fetchResult)
+  let statusChange =
+        (invalidZipCodeChange $> "Not a valid zip code") <>
+        (validZipCodeChange $> "Loading ...") <>
+        (fetchResult <#> hush # filterJust)
   status <- stepper "" statusChange
-  pure { status }
-
-zipView { status } =
-  E.div_ (
-    E.span_ (E.text "Please type a valid US zip code: ") </>
-    E.input (static { placeholder: "Zip code" }) `use` (\o -> { zipCode: o.inputValue }) </>
-    E.br </>
-    E.span_ (E.textB status)
-  )
-
-app :: Component {} AppModelOut
-app = modelView zipModel zipView
+  ( E.div {} (
+      E.span {} (E.text "Please type a valid US zip code: ") </>
+      E.input (static { placeholder: "Zip code" }) `use` (\o -> { zipCode: o.value }) </>
+      E.br </>
+      E.span {} (E.textB status)
+    )
+  ) `output` {}
 
 main :: Effect Unit
 main = runComponent "#mount" app
